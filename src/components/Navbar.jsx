b7f2792537a5a8a319473e09d14a8d17e058417c
Franco Armani <91090081+franarmani@users.noreply.github.com>
2024-12-10 12:47:05 -0300

    Commit 2
    Estas mejoras aseguran que la aplicación sea funcional, accesible y fácil de usar para cualquier usuario, además de ofrecer una experiencia visual agradable.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,28 @@
+import { useState } from 'react';
 import './Navbar.css'; 
 
 const Navbar = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
   return (
     <nav className="navbar">
       <div className="navbar-container">
-        {}
+        {/* Logo */}
         <div className="navbar-logo">Weather Fast</div>
 
-        {}
-        <ul className="navbar-links">
+        {/* Botón hamburguesa */}
+        <div className="navbar-toggle" onClick={toggleMenu}>
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
+
+        {/* Enlaces del menú */}
+        <ul className={`navbar-links ${menuOpen ? 'active' : ''}`}>
           <li><a href="https://www.linkedin.com/in/franco-armani-993a36234/" target="_blank" rel="noopener noreferrer">LinkedIn</a></li>
           <li><a href="https://www.instagram.com/fran.armani_/" target="_blank" rel="noopener noreferrer">Instagram</a></li>
           <li><a href="https://github.com/franarmani" target="_blank" rel="noopener noreferrer">GitHub</a></li>
--- src/components/WeatherApp.jsx
+++ src/components/WeatherApp.jsx
@@ -1,93 +1,171 @@
-import { useState } from 'react';
-import axios from 'axios';
-import Navbar from './Navbar';  // Importamos el Navbar
-import './WeatherApp.css';
+import { useEffect, useState } from "react";
+import axios from "axios";
+import Navbar from "./Navbar";
+import "./WeatherApp.css";
 
 const WeatherApp = () => {
-  const [city, setCity] = useState('');
+  const [city, setCity] = useState("");
   const [weatherData, setWeatherData] = useState(null);
+  const [hourlyForecast, setHourlyForecast] = useState([]);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
+  const [searchedCity, setSearchedCity] = useState("");
 
-  const handleSearch = () => {
-    if (!city) return; // Evitar la consulta si la ciudad está vacía
+  const formatLocalTime = (date) => {
+    const options = {
+      hour: "2-digit",
+      minute: "2-digit",
+      hour12: false,
+    };
+    const localTime = new Date(date).toLocaleString("es-ES", options);
+    return localTime;
+  };
 
+  const fetchWeatherByGeolocation = (latitude, longitude) => {
     setLoading(true);
     setError(null);
-
     axios
-      .get(`https://api.weatherapi.com/v1/current.json?key=60feeb2fb4c740adb1a132532240912&q=${city}&lang=es`)
+      .get(
+        `https://api.weatherapi.com/v1/forecast.json?key=60feeb2fb4c740adb1a132532240912&q=${latitude},${longitude}&lang=es`
+      )
       .then((response) => {
-        setWeatherData(response.data);
-        setLoading(false);
+        handleWeatherData(response.data);
       })
-      .catch(() => {
-        setError('No se pudo obtener el clima. Intenta de nuevo.');
+      .catch((err) => {
+        console.error(err);
+        setError("No se pudo obtener el clima para tu ubicación.");
         setLoading(false);
       });
   };
 
-  const handleKeyPress = (e) => {
-    if (e.key === 'Enter') {
-      handleSearch();
+  const handleGeolocation = () => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          const { latitude, longitude } = position.coords;
+          fetchWeatherByGeolocation(latitude, longitude);
+        },
+        () => {
+          setError("No se pudo acceder a tu ubicación.");
+        }
+      );
+    } else {
+      setError("La geolocalización no está soportada en tu navegador.");
     }
   };
 
-  const getWeatherGif = (condition) => {
-    switch (condition) {
-      case 'Sunny':
-        return './assets/sol.png';
-      case 'Cloudy':
-        return './assets/nublado.png';
-      case 'Rain':
-        return './assets/lluvia.png';
-      case 'Thunderstorm':
-        return './assets/tormenta.png';
-      case 'Snow':
-        return './assets/nieve.png';
-      default:
-        return './assets/normal.png'; 
+  useEffect(() => {
+    handleGeolocation();
+  }, []);
+
+  const handleSearch = () => {
+    const normalizedCity = city.trim().toLowerCase();
+
+    if (!normalizedCity) {
+      setError("Por favor, ingresa un nombre de ciudad válido.");
+      return;
     }
+
+    setLoading(true);
+    setError(null);
+    setSearchedCity(city);
+
+    axios
+      .get(
+        `https://api.weatherapi.com/v1/forecast.json?key=60feeb2fb4c740adb1a132532240912&q=${normalizedCity}&lang=es`
+      )
+      .then((response) => {
+        handleWeatherData(response.data);
+      })
+      .catch((err) => {
+        console.error(err);
+        setError(
+          "No se pudo obtener el clima para la ciudad especificada. Verifica el nombre e intenta nuevamente."
+        );
+        setLoading(false);
+      });
+  };
+
+  const handleWeatherData = (data) => {
+    setWeatherData(data.current);
+    const forecastData = data.forecast.forecastday[0].hour;
+
+    const currentTime = new Date();
+    const futureHours = forecastData.filter((hour) => {
+      const hourTime = new Date(hour.time);
+      return hourTime > currentTime;
+    });
+
+    const hourlyForecastLocal = futureHours.map((hour) => {
+      const localHour = formatLocalTime(new Date(hour.time));
+      return { ...hour, localTime: localHour };
+    });
+
+    setHourlyForecast(hourlyForecastLocal.slice(0, 5));
+
+    const cityName = data.location.name;
+    setSearchedCity(cityName);
+
+    setLoading(false);
   };
 
   return (
     <div className="weather-app">
-      <Navbar />  {/* Colocamos el Navbar en la parte superior */}
-      
+      <Navbar />
       <div className="weather-container">
-        {/* Título de la página */}
         <h1 className="page-title">Weather Fast</h1>
-
-        {/* Contenedor de búsqueda */}
         <div className="search-container">
           <input
             type="text"
             className="city-input"
             value={city}
             onChange={(e) => setCity(e.target.value)}
-            onKeyPress={handleKeyPress}
             placeholder="Ingresa una ciudad"
           />
-          <button className="search-button" onClick={handleSearch}>Buscar</button>
+          <button className="search-button" onClick={handleSearch}>
+            Buscar
+          </button>
         </div>
-
         {loading && <div className="loading-text">Cargando...</div>}
         {error && <div className="error-text">{error}</div>}
 
         {weatherData && (
-          <div className="weather-card">
-            <h2 className="weather-title">Clima en {weatherData.location.name}</h2>
-            <div className="city-name">{weatherData.location.name}, {weatherData.location.country}</div>
-            <img
-              className="weather-icon"
-              src={getWeatherGif(weatherData.current.condition.text)}
-              alt={weatherData.current.condition.text}
-            />
-            <div className="temperature">{weatherData.current.temp_c}°C</div>
-            <div className="weather-condition">{weatherData.current.condition.text}</div>
-            <div className="info-text">Viento: {weatherData.current.wind_kph} km/h</div>
-            <div className="info-text">Humedad: {weatherData.current.humidity}%</div>
-          </div>
+          <>
+            <div className="weather-card">
+              <h2 className="weather-title">
+                {searchedCity ? `Clima en ${searchedCity}` : "Clima en tu ubicación"}
+              </h2>
+              <img
+                className="weather-icon"
+                src={`https:${weatherData.condition.icon}`}
+                alt={weatherData.condition.text}
+              />
+              <div className="temperature">{weatherData.temp_c}°C</div>
+              <div className="weather-condition">
+                {weatherData.condition.text}
+              </div>
+              <div className="info-text">Viento: {weatherData.wind_kph} km/h</div>
+              <div className="info-text">Humedad: {weatherData.humidity}%</div>
+            </div>
+
+            <div className="hourly-forecast">
+              <h3>Clima en las próximas horas:</h3>
+              <div className="hourly-details">
+                {hourlyForecast.map((hour, index) => (
+                  <div className="hourly-item" key={index}>
+                    <div className="hour">{hour.localTime}</div>
+                    <img
+                      className="hourly-icon"
+                      src={`https:${hour.condition.icon}`}
+                      alt={hour.condition.text}
+                    />
+                    <div className="temperature">{hour.temp_c}°C</div>
+                    <div className="weather-condition">{hour.condition.text}</div>
+                  </div>
+                ))}
+              </div>
+            </div>
+          </>
         )}
       </div>
     </div>
